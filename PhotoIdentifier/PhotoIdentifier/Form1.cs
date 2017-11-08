2bc9337228ff826ded815171b7ba8e6885d83d23
Raccoonmaster <raccoonmaster@users.noreply.github.com>
2017-11-08 16:20:15 +0100

    base

---
+++ PhotoIdentifier/PhotoIdentifier/Form1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.ProjectOxford.Face;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+namespace PhotoIdentifier {
+    public partial class Form1:Form {
+
+        private readonly IFaceServiceClient faceServiceClient = new FaceServiceClient("aeb4502d493444c0bf75969ad78f9e99", "https://westeurope.api.cognitive.microsoft.com/face/v1.0");
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            
+    }
+    }
+}
