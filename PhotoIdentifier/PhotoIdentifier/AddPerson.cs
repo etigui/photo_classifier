c95c424e4b7de7488babdedb77fa1011efaea7b1
Raccoonmaster <raccoonmaster@users.noreply.github.com>
2017-11-20 20:58:22 +0100

    Before remove db

--- PhotoIdentifier/PhotoIdentifier/AddPerson.cs
+++ PhotoIdentifier/PhotoIdentifier/AddPerson.cs
@@ -5,8 +5,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Security;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,10 +21,13 @@ namespace PhotoIdentifier {
 
         #region Vars
         private Person person;
+        private string person_app_path = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "person");
+        private string name_path = "";
+        private static Random rnd = new Random();
         #endregion
 
         private void AddPerson_Load(object sender, EventArgs e) {
-            person = new Person();
+            //person = new Person();
         }
 
         #region Change thumbnails size
@@ -137,7 +142,9 @@ namespace PhotoIdentifier {
 
         private void BT_add_Click(object sender, EventArgs e) {
 
-            // Check if only letter in the textbox
+            add_person(TB_name.Text);
+
+            /*// Check if only letter in the textbox
             if(TB_name.Text.All(Char.IsLetter)) {
 
                 // Check if textbox is not empty
@@ -164,7 +171,7 @@ namespace PhotoIdentifier {
                 }
             } else {
                 MessageBox.Show("The name must me only letter", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            }*/
         }
 
         private void TB_name_TextChanged(object sender, EventArgs e) {
@@ -179,10 +186,96 @@ namespace PhotoIdentifier {
         #endregion
 
         #region test
-        private void add_person_photos() {
-            foreach(ImageListViewItem item in ILV_photos.Items) {
 
-            }
+        public bool add_person(string name) {
+            if(!add_dir_name(name))
+                return false;
+            if(!copy_photos())
+                return false;
+            return true;
+        }
+
+        private bool add_dir_name(string name) {
+            try {
+
+                // Dir will be created to store the photos
+                name_path = $"{name}_{get_random(10)}";
+
+                // Create specific directory
+                string dir = Path.Combine(person_app_path, name_path);
+                if(!Directory.Exists(dir)) {
+                    Directory.CreateDirectory(dir);
+                } else {
+
+                    // If fail create new dir
+                    name_path = $"{name}_{get_random(20)}";
+                    Directory.CreateDirectory(Path.Combine(person_app_path, name_path));
+                }
+                return true;
+            } catch { return false; }
+        }
+
+        private bool copy_photos() {
+            try {
+                // Get all photos from imagelistview
+                foreach(ImageListViewItem item in ILV_photos.Items) {
+
+                    // Get source file
+                    string source = item.FileName;
+
+                    // Copy photo to specific directory
+                    if(File.Exists(source)) {
+
+                        // Get hash from source file
+                        string hash = get_hsah256_file(source);
+
+                        // Dest file
+                        string source_ext = Path.GetFileNameWithoutExtension(item.FileName);
+                        string dest = Path.Combine(person_app_path, name_path, (hash + Path.GetExtension(item.FileName)));
+
+                        // Check if hash could be calculated
+                        if(hash != "") {
+                            if(!File.Exists(dest))
+                                File.Copy(source, dest, true);
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+                return true;
+            } catch { MessageBox.Show("ddd"); return false; }
+        }
+
+        /// <summary>
+        /// Get random string
+        /// </summary>
+        /// <param name="length">Random length</param>
+        /// <returns></returns>
+        private string get_random(int length) {
+            const string chars = "abcdefghijklmnopqrstuvwxyz";
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[rnd.Next(s.Length)]).ToArray());
+        }
+
+        private string get_hsah256_file(string path) {
+            string hash = "";
+            try {
+
+                // Create a fileStream for the file.
+                using(FileStream fs = new FileStream(path, FileMode.Open)) {
+
+                    // Initialize a SHA256 hash object.
+                    SHA256 hash_256 = SHA256Managed.Create();
+
+                    // Be sure it's positioned to the beginning of the stream.
+                    fs.Position = 0;
+
+                    // Compute the hash of the fileStream.
+                    hash = BitConverter.ToString(hash_256.ComputeHash(fs)).Replace("-", String.Empty);
+                }
+                return hash;
+            } catch { return ""; }
         }
         #endregion
     }
